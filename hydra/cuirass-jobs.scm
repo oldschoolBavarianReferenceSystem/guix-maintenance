oldschoolBavarianReferenceSystem/guix-maintenance

commit 9779050ef9dfde97370972fb84abab097d498d90

hydra: bayfront: Adjust the binary tarball job on Cuirass.
* hydra/cuirass-jobs.scm (tarball-job): Use 'self-contained-tarball'
from (guix scripts pack).

hydra/cuirass-jobs.scm
@@ -29,6 +29,7 @@
              (guix store)
              (guix grafts)
              (guix packages)
+             (guix profiles)
              (guix derivations)
              (guix monads)
              ((guix licenses)
@@ -39,9 +40,11 @@
              (gnu packages commencement)
              (gnu packages guile)
              (gnu packages make-bootstrap)
+             (gnu packages package-management)
              (gnu system)
              (gnu system vm)
-             (gnu system install)
+             ((guix scripts pack)
+              #:select (lookup-compressor self-contained-tarball))
              (srfi srfi-1)
              (ice-9 match))
 
@@ -120,8 +123,14 @@ for TARGET on SYSTEM."
                           (run-with-store store
                             (mbegin %store-monad
                               (set-guile-for-build (default-guile))
-                              (self-contained-tarball))
-                            #:system system))))
+                              (>>= (profile-derivation
+                                    (packages->manifest (list guix)))
+                                   (lambda (profile)
+                                     (self-contained-tarball
+                                      "guix-binary" profile
+                                      #:compressor
+                                      (lookup-compressor "lzip"))))))
+                          #:system system)))
       (#:description . "Stand-alone binary Guix tarball")
       (#:long-description . "This is a tarball containing binaries of Guix
 and all its dependencies, and ready to be installed on non-GuixSD

commit 4e1f529a0071d4f23f9da617e0ab0e26e41fe804

hydra: bayfront: Fix binary tarball job for Cuirass.
Fixes a typo introduced in 9779050ef9dfde97370972fb84abab097d498d90.

* hydra/cuirass-jobs.scm (tarball-job): #:system is an argument of
'run-with-store'.

hydra/cuirass-jobs.scm
@@ -129,8 +129,8 @@ for TARGET on SYSTEM."
                                      (self-contained-tarball
                                       "guix-binary" profile
                                       #:compressor
-                                      (lookup-compressor "lzip"))))))
-                          #:system system)))
+                                      (lookup-compressor "lzip")))))
+                            #:system system))))
       (#:description . "Stand-alone binary Guix tarball")
       (#:long-description . "This is a tarball containing binaries of Guix
 and all its dependencies, and ready to be installed on non-GuixSD